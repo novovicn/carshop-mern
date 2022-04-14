6d39ab47bc62fc44c3963cc11b39c6f1375db29f
novovicn <novovicnenad96@gmail.com>
2022-04-14 18:52:24 +0200

    Prepare deploy

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,10 @@
 const express = require('express');
-require('dotenv').config(); 
-const carRoutes = require('./routes/carRoutes')
-const userRoutes = require('./routes/userRoutes')
+const path = require('path');
+require('dotenv').config();
+const carRoutes = require('./routes/carRoutes');
+const userRoutes = require('./routes/userRoutes');
 const connectDB = require('./config/db');
-const {errorMiddleware} = require('./middlewares/errorMiddleware');
+const { errorMiddleware } = require('./middlewares/errorMiddleware');
 
 const app = express();
 
@@ -13,6 +14,18 @@ app.use(express.json());
 
 app.use('/api/users', userRoutes);
 app.use('/api/cars', carRoutes);
+
+// Serve frontend
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../frontend/build')));
+
+  app.get('*', (req, res) =>
+    res.sendFile(
+      path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')
+    )
+  );
+}
 
 app.use(errorMiddleware);
 const PORT = process.env.PORT;
