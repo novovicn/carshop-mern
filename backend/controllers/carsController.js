15702255aafa8b4d07246126c227593372225996
novovicn <novovicnenad96@gmail.com>
2022-04-17 18:39:29 +0200

    added backend pagination flow

--- backend/controllers/carsController.js
+++ backend/controllers/carsController.js
@@ -4,11 +4,11 @@ const Car = require('../models/carModel');
 const addCar = asyncHandler(async (req, res) => {
   const { brand, model, year, image, mileage, price } = req.body;
 
-  if(!brand || !model ||!year || !mileage || !price){
-    res.status(400)
+  if (!brand || !model || !year || !mileage || !price) {
+    res.status(400);
     throw new Error('Please populate all required fields');
   }
-  
+
   const car = await Car.create({
     owner: req.userId,
     brand,
@@ -19,36 +19,45 @@ const addCar = asyncHandler(async (req, res) => {
     price,
   });
 
-  if(car){
-      res.status(201).json(car)
-  }else{
-      res.status(400)
-      throw new Error('Invalid data!');
+  if (car) {
+    res.status(201).json(car);
+  } else {
+    res.status(400);
+    throw new Error('Invalid data!');
   }
 });
 
 const getCars = asyncHandler(async (req, res) => {
-  const cars = await Car.find().sort({createdAt: -1});
-  res.status(200).json(cars);
+  const pageSize = 6;
+  const page = req.query.page || 1;
+
+  const count = await Car.countDocuments({});
+  const pages = Math.ceil(count / pageSize);
+  const cars = await Car.find()
+    .sort({ createdAt: -1 })
+    .limit(pageSize)
+    .skip(pageSize * (page - 1));
+
+  res.status(200).json(cars, page, pages);
 });
 
 const getSingleCar = asyncHandler(async (req, res) => {
   const car = await Car.findById(req.params.id).populate('owner', 'id name');
-  
-  if(car){
+
+  if (car) {
     res.status(200).json(car);
-  }else{
-    res.status(400)
+  } else {
+    res.status(400);
     throw new Error('Car not found');
   }
 });
 
 const deleteCar = asyncHandler(async (req, res) => {
-  const removedCar = await Car.deleteOne({_id: req.params.id})
-  if(removedCar.deletedCount > 0){
+  const removedCar = await Car.deleteOne({ _id: req.params.id });
+  if (removedCar.deletedCount > 0) {
     res.status(200).json('Car removed successfully');
-  }else{
-    res.status(400)
+  } else {
+    res.status(400);
     throw new Error('Car not found');
   }
 });
@@ -57,5 +66,5 @@ module.exports = {
   addCar,
   getCars,
   getSingleCar,
-  deleteCar
+  deleteCar,
 };
