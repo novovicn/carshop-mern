9bf89c824d6d8f547a125d3d2298ffbce786ef14
novovicn <novovicnenad96@gmail.com>
2022-05-03 13:21:43 +0200

    added brand search and changed stylings

--- frontend/src/components/Search.js
+++ frontend/src/components/Search.js
@@ -10,7 +10,7 @@ function Search(props) {
   const handleSubmit = (e) => {
     e.preventDefault();
     props.onSearch(keyword);
-  }
+  };
 
   return (
     <>
@@ -22,34 +22,19 @@ function Search(props) {
       </div>
       {searchBoxVisible && (
         <form className="search" onSubmit={handleSubmit}>
-          <div className="search__price">
-            <label htmlFor="search__price--lowest">Price </label>
+          <div className="search__brand">
             <input
-              type="number"
-              name="search__price--lowest"
-              className="search__price--lowest"
-              placeholder="from"
-            />
-            <input
-              type="number"
-              name="search__price--highest"
-              className="search__price--highest"
-              placeholder="to"
+              type="text"
+              name="search-brand"
+              id="search-brand"
+              placeholder="Brand"
+              onChange={(e) => setKeyword(e.target.value)}
             />
             <input
               type="submit"
               className="search__price--submit"
               value="Search"
             ></input>
-          </div>
-          <div className="search__brand">
-            <input
-              type="text"
-              name="search-brand"
-              id="search-brand"
-              placeholder="Brand and model"
-              onChange={e => setKeyword(e.target.value)}
-            />
             <i className="fas fa-search"></i>
           </div>
         </form>
